7a53f0d48be38019a67e621a163dc9568e0d5052
Guilherme D'aguano <guidaguano@gmail.com>
2024-10-08 16:54:21 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -1,17 +1,27 @@
-from itertools import filterfalse
+import random
 
 
 def jogar():
 
-    print("**********************************")
-    print("***Bem vindo ao jogo de Forca!***")
-    print("**********************************")
+    hub()
 
-    palavra_secret = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+    palavra_secret = palavras[numero].upper()
+
+    letras_acertadas = ["_" for letra in palavra_secret]
 
     enforcou = False
     acertou = False
+    erro = 0
 
     print (letras_acertadas)
 
@@ -20,18 +30,34 @@ def jogar():
         chute = input("Qual letra: ")
         chute = chute.strip()
 
+        if(chute.upper() in palavra_secret.upper()):
+            posicao = 0
+            for letra in palavra_secret:
+                if(chute.upper() == letra.upper()):
+                    letras_acertadas[posicao] = letra
+                posicao = posicao + 1
+        else:
+            erro += 1
 
-        posicao = 0
-        for letra in palavra_secret:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[posicao] = letra
-            posicao = posicao + 1
-
+        enforcou = erro == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
+    if (acertou):
+        print("Você acertou. Parabéns!")
+        print(palavra_secret)
+    else:
+        print("Que pena. Você perdeu!")
+        print("A palavra era: {}". format(palavra_secret))
     print("---------------------")
     print("Fim do jogo! Agradece")
     print("---------------------")
 
 if (__name__ == "__main__"):
     jogar()
+
+
+    def hub():
+        print("**********************************")
+        print("***Bem vindo ao jogo de Forca!***")
+        print("**********************************")
